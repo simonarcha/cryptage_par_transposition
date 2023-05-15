d44e4d52b963747373c7c206ba0ba5f809cbe776
Simon Archambeau <210303@site.asty.moulin.be>
2023-05-15 23:42:14 +0200

    ajout du programme de cryptage sur git hub

---
+++ 5tti_CryptageTransposition_SimonA/MethodeDuProjet.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cryptage
+{
+    public struct MethodeDuProjet
+    {
+        public void Entreecle(out char[] Tcle)
+        {
+            Console.Clear();
+            bool ok = true;
+            string cle = ("");
+            Tcle = cle.ToCharArray(); // sert a verifier si c'est entrer correctement en lettre uniquement
+            do
+            {
+                Console.WriteLine("Veuillez entrer la clé Alphanumérique : ");
+                cle = Console.ReadLine().ToUpper();
+                if (IsAlphaNumeric(cle))
+                {
+                    Tcle = cle.ToCharArray();
+                    ok = true;
+                }
+                else
+                {
+                    Console.WriteLine("La clé doit être uniquement alphanumérique");
+                    Console.ReadKey();
+                    ok = false;
+                }
+            } while (!ok);
+
+        }
+        public void Entreephrase(out char[] Tphrase)
+        {
+            Console.Clear();
+            bool ok = true;
+            string phr = ("");
+            Tphrase = phr.ToCharArray(); // sert a ce que les espaces entrer soient remplacer par ""
+            do
+            {
+                Console.WriteLine("Veuillez entrer la phrase a crypter : ");
+                phr = Console.ReadLine().ToUpper();
+                phr = phr.Replace(" ", String.Empty);
+                if (IsAlphaNumeric(phr))
+                {
+                    Tphrase = phr.ToCharArray();
+                    ok = true;
+                }
+                else
+                {
+                    Console.WriteLine("La phrase doit être uniquement alphanumérique");
+                    Console.ReadKey();
+                    ok = false;
+                }
+            } while (!ok);
+        }
+        public static bool IsAlphaNumeric(string s)
+        {
+            foreach (char c in s)
+            {
+                if (!(c >= 'A' && c <= 'Z') &&
+                        !(c >= 'a' && c <= 'z'))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public static char[] Trieetnum(char[] Temp)
+        {
+            char[] Tcletemp = new char[Temp.Length];
+            char[] Tsort = new char[Temp.Length];
+            char[] Tnum = new char[Temp.Length];
+            Array.Copy(Temp, Tcletemp, Temp.Length);
+            Array.Copy(Temp, Tsort, Temp.Length);
+            int index = 0;
+            Array.Sort(Tsort);  // fonction de tri
+            int j = (int)Tsort[0] - 64; // le numéro du premier caractère (a=1,b=2, etc...) de la clé
+            for (int i = 0; i < Tsort.Length; i++) // j'attribue la valeur numérique à la lettre n° ASCII
+            {
+                Tnum[i] = (char)(48 + j + i);
+            }
+            int k = 0;
+            foreach (char c in Tcletemp)
+            {
+                index = Array.IndexOf(Tsort, c);
+                Tsort[index] = '\0';
+                Temp[k] = Tnum[index];
+                k++;
+
+            }
+            return Temp;
+        }
+        public void AfficherMatrice(in char[,] matrice)
+        {
+            int lignes = matrice.GetLength(0);
+            int colonnes = matrice.GetLength(1);
+
+            for (int i = 0; i < lignes; i++)
+            {
+                for (int j = 0; j < colonnes; j++)
+                {
+                    Console.Write(matrice[i, j] + "  "); // Affiche la valeur de chaque élément de la matrice avec deux espaces entre chaque élément
+                }
+                Console.Write("\n"); // Passe à la ligne suivante
+            }
+        }
+        public void Affichephasecodee(in char[,] matrice, in char[] Tcle)
+        {
+            string phrasecodee = "";
+            int lignes = matrice.GetLength(0);
+            int colonnes = matrice.GetLength(1);
+
+            for (int i = 49; i <= colonnes + 48; i++)
+            {
+                int k = Array.IndexOf(Tcle, (char)i);
+                for (int j = 2; j < lignes; j++)
+                {
+                    phrasecodee = phrasecodee + matrice[j, k];
+                }
+
+                phrasecodee = phrasecodee + " ";
+            }
+            Console.Write(phrasecodee);
+        }
+        public  void Creematcrypt(in char[] Tcle, in char[] Tphrase, out char[,] MatCrypt)
+        {
+            int n = Tcle.Length;
+            int m = Tphrase.Length;
+            int div = m / n;
+            if (m % n > 0) { div++; }
+            MatCrypt = new char[div + 2, n];
+            char[] Tcleinit = new char[n];
+            Array.Copy(Tcle, Tcleinit, Tcle.Length);
+            Trieetnum(Tcle);
+            for (int i = 0; i < n; i++) { MatCrypt[0, i] = Tcleinit[i]; }
+            for (int i = 0; i < n; i++) { MatCrypt[1, i] = Tcle[i]; }
+            int j = 2;
+            int k = 0;
+            for (int i = 0; i < m;)
+            {
+                MatCrypt[j, k] = Tphrase[i];
+                k++;
+                i++;
+                if (k % n == 0) { j++; k = 0; }
+            }
+        }
+    }
+}
---
+++ 5tti_CryptageTransposition_SimonA/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace cryptage
+{
+    internal class Program
+    {
+        /*
+-------------------------------------------------------------------------------------------------------
+            4.La méthode de Cryptage par transposition:
+            Les méthodes de cryptographie par transposition sont celles pour lesquelles on chiffre le
+            message en permutant l'ordre des lettres du message suivant des règles bien définies.
+            Autrement dit, on produit une anagramme du message initial.
+            Effectuons un chiffrement par transposition rectangulaire :
+            On commence par se mettre d'accord sur un mot-clé, par exemple le mot BIBMATH. On classe
+            les lettres de ce mot et on attribue alors à chaque lettre son numéro dans l'ordre alphabétique.
+            Ainsi, on donne à A le numéro 1, au premier B le numéro 2, au deuxième B le numéro 3, au H le
+            numéro 4, etc....On crée ensuite un tableau de la façon suivante :
+             La première ligne est constituée par les lettres de la clé;
+             La deuxième ligne est constituée par les numéros qui leur sont associés;
+             On complète ensuite le tableau en le remplissant avec les lettres du message à
+            chiffrer.On écrit sur chaque ligne autant de lettres que de lettres dans la clé.
+            Éventuellement, la dernière ligne n'est pas complète.
+            Exemple: "JE SUIS ELEVE AU CENTRE ASTYMOULIN".
+            B I B M A T H
+            2 5 3 6 1 7 4
+            J E S U I S E
+            L E V E A U C
+            E N T R E A S
+            T Y M O U L I
+            N
+
+            Ensuite, on écrit d'abord le contenu de la colonne numérotée 1, puis le contenu de la colonne
+            numérotée 2, etc...Le message chiffré obtenu est alors :
+            IAEU JLETN SVTM ECSI EENY UERO SUAL
+            Pour le déchiffrement, à vous de jouer...
+------------------------------------------------------------------------------------------------------------ 
+        */
+        static void Main(string[] args)
+        {
+            MethodeDuProjet MesOutils = new MethodeDuProjet();
+            MesOutils.Entreecle(out char[] Tcle);
+            MesOutils.Entreephrase(out char[] Tphrase);
+            MesOutils.Creematcrypt(in Tcle, in Tphrase, out char[,] MatCrypt);
+            MesOutils.AfficherMatrice(MatCrypt);
+            Console.ReadKey();
+            MesOutils.Affichephasecodee(in MatCrypt,in Tcle);
+            Console.ReadKey();
+        }
+    }
+}
